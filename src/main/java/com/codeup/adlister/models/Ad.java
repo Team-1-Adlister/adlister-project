6c9fb5b0bbc253ff03ab718796043895fb04aa66
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-10-28 11:51:49 -0500

    added ads change

--- src/main/java/com/codeup/adlister/models/Ad.java
+++ src/main/java/com/codeup/adlister/models/Ad.java
@@ -11,13 +11,6 @@ public class Ad {
     private String description;
     private Timestamp date;
 
-//    public Ad(long id, long userId, String title, String description) {
-//        this.id = id;
-//        this.userId = userId;
-//        this.title = title;
-//        this.description = description;
-//    }
-
     public Ad(long userId, String title, String description) {
         this.userId = userId;
         this.title = title;
